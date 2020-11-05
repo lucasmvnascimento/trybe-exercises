7c8bd82cc1b733e4493c57556fed35ce0ad6dbf4
Lucas Nascimento <lucasmvnascimento@gmail.com>
2020-11-05 12:26:41 -0300

    Final Exercicio 1

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -14,3 +14,21 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  let dezDaysList = [29,30];
+  for (let i=1; i<32; i+=1){
+    dezDaysList.push(i);
+  }
+  let days = document.querySelector("#days");
+  for (let i=0; i<dezDaysList.length; i+=1){
+    let day = document.createElement("li");
+    let aux = dezDaysList[i];
+    day.innerText = aux;
+    if (aux==24 || aux==25 || aux==31) {
+      day.className = "day holiday";
+    } else if (aux==4 || aux==11 || aux==18 || aux==25) {
+      day.className = "day friday";
+    } else {
+      day.className = "day";
+    }
+    days.appendChild(day);
+    }
