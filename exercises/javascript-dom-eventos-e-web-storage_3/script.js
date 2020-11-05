5017bae149d937d19b343f08e353b6e252637838
Lucas Nascimento <lucasmvnascimento@gmail.com>
2020-11-05 15:29:39 -0300

    Exercicios 4 e 5

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -24,10 +24,12 @@ function createDaysOfTheWeek() {
     let day = document.createElement("li");
     let aux = dezDaysList[i];
     day.innerText = aux;
-    if (aux==24 || aux==25 || aux==31) {
+    if (aux==24 || aux==31) {
       day.className = "day holiday";
-    } else if (aux==4 || aux==11 || aux==18 || aux==25) {
+    } else if (aux==4 || aux==11 || aux==18) {
       day.className = "day friday";
+    } else if (aux == 25){
+      day.className = "day holiday friday";
     } else {
       day.className = "day";
     }
@@ -47,18 +49,50 @@ criaBotaoFeriados(feriados);
 // Exercicio 3
 diasFeriados = document.querySelectorAll (".holiday");
 botaoFeriados = document.querySelector(".btn-holiday");
-let estadoAtual = 0;
+let estadoAtualFeriado = 0;
 function mudaCorFeriados () {
-  if (estadoAtual == 0){
+  if (estadoAtualFeriado == 0){
     for (let i=0; i<diasFeriados.length; i+=1){
       diasFeriados[i].style.backgroundColor = "blue";
     }
-    estadoAtual = 1;
-  }else if (estadoAtual == 1) {
+    estadoAtualFeriado = 1;
+  }else if (estadoAtualFeriado == 1) {
     for (let i=0; i<diasFeriados.length; i+=1){
       diasFeriados[i].style.backgroundColor = "rgb(238,238,238)";
     }
-    estadoAtual = 0;
+    estadoAtualFeriado = 0;
   }
 }
 botaoFeriados.addEventListener('click', mudaCorFeriados);
+
+// Exercicio 4
+function criaBotaoSextas () {
+  let botaoSextas = document.createElement("button");
+  botaoSextas.id = "btn-friday";
+  botaoSextas.innerText = "Sexta-Feira";
+  document.querySelector(".buttons-container").appendChild(botaoSextas);
+}
+criaBotaoSextas();
+
+// Exercicio 5
+let sextas = document.querySelectorAll(".friday");
+let sextasOriginais = [];
+for (let i=0; i<sextas.length; i+=1){
+  sextasOriginais.push(sextas[i].innerText);
+}
+let botaoSextas = document.querySelector("#btn-friday");
+let estadoAtualSexta = 0;
+function mudaCorSexta () {
+  if (estadoAtualSexta == 0){
+    for(let i=0; i<sextas.length; i+=1) {
+      sextas[i].innerText = "SEXTOU!";
+    }
+    estadoAtualSexta = 1;
+  } else if (estadoAtualSexta == 1) {
+    for(let i=0; i<sextas.length; i+=1) {
+      sextas[i].innerText = sextasOriginais[i];
+    }
+    estadoAtualSexta = 0;
+  }
+}
+botaoSextas.addEventListener('click',mudaCorSexta);
